9c73572fb52e005451b1ec642373041715c0eead
dhruvsagar <dhruv.sagar@gatech.edu>
2016-11-29 23:08:48 -0500

    updates database on start of game
    sign out button needs to be added

--- student.js
+++ student.js
@@ -1,32 +1,39 @@
 $(document).ready(function() {
-  var database = firebase.database();
+    var database = firebase.database();
 
-  $("#sign-in").submit(function(event) {
-  	firebase.auth().signInAnonymously().catch(function(error) {
-  	  // Handle Errors here.
-  	  var errorCode = error.code;
-  	  var errorMessage = error.message;
-  	  // ...
-  	});
-    console.log(event.target);
-    event.preventDefault();
-  });
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      firebase.database().ref('students/' + uid).set({
-          name: $('#name').val(),
-          fact: $('#fact').val()
-      });
-      console.log(isAnonymous);
-      console.log(uid);
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-    // ...
-  });
+    $("#sign-in").submit(function(event) {
+        firebase.auth().signInAnonymously().catch(function(error) {
+          // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+          // ...
+        });
+        console.log(event.target);
+        event.preventDefault();
+    });
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        // User is signed in.
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        firebase.database().ref('students/' + uid).set({
+            name: $('#name').val(),
+            fact: $('#fact').val()
+        });
+        console.log(isAnonymous);
+        console.log(uid);
+        // ...
+        } else {
+        // User is signed out.
+        // ...
+        }
+        // ...
+    });
+    $("#sign-out").click(function(event) {
+        firebase.auth().signOut().then(function() {
+            console.log('Signed Out');
+        }, function(error) {
+            console.error('Sign Out Error', error);
+        });
+    });
 });
--- teacher.js
+++ teacher.js
@@ -1,9 +1,59 @@
 $(document).ready(function() {
+  $("#startGameButton").hide();
   var database = firebase.database();
   var gamesRef = firebase.database().ref('games/').once('value').then(function(snapshot) {
-    console.log(snapshot.val());
-    for (i = 1; i < snapshot.val().length; i++) {
-      $('#games').append("<li>" + snapshot.val()[i].name + "</li>")
-    }
+    snapshot.forEach(function(childSnapShot) {
+    	$('#games').append('<li onclick="gameSelected(event)" data-id = "' + childSnapShot.key + '" data-gametype= "' + childSnapShot.val().type + '">' + childSnapShot.val().name + '</li>');
+    });  
   });
 });
+
+function gameSelected(event) {
+	var gameId = $(event.target).data().id;
+	var gameType = $(event.target).data().gametype;
+	console.log(gameId);
+	console.log(gameType);
+	$("#startGameButton").show();
+	$("#startGameButton").click(function(event) {
+		$("#games").hide();
+		$("#startGameButton").hide();
+		startGame(gameId, gameType);
+	});
+}
+
+function startGame(gameid, gameType) {
+	switch(gameType) {
+		case "matching" :
+			matchingGame(gameid);
+			break;
+		default:
+			console.log("FUCCKKK");
+	}
+}
+
+function matchingGame(gameid) {
+	var words = {};
+	firebase.database().ref('games/' + gameid + '/questions/').once('value').then(function(snapshot) {
+		snapshot.forEach(function(childSnapShot) {
+			words[childSnapShot.key] = childSnapShot.val();
+		});
+	});
+	var playersRef = firebase.database().ref('students/').once('value').then(function(snapshot) {
+		counter = 0;
+		key = true;
+		keys = Object.keys(words);
+		//debugger;
+		snapshot.forEach(function(childSnapShot) {
+			if (key) {
+				var input = keys[counter];
+				firebase.database().ref('students/' + childSnapShot.key).update({word : input,});
+				key = false;
+			} else {
+				var input = words[keys[counter]];
+				firebase.database().ref('students/' + childSnapShot.key).update({word : input,});
+				key = true;
+				counter++;
+			}
+		});
+	});
+}
