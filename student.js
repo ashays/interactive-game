e6b9679088c58b0c95117af0a09a90e8b1b3e6e3
Ashay Sheth <ashay.sheth@gmail.com>
2016-11-30 01:03:51 -0500

    added student showing word and panels and some styling

--- student.js
+++ student.js
@@ -11,9 +11,14 @@ $(document).ready(function() {
         console.log(event.target);
         event.preventDefault();
     });
+
+
+
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
         // User is signed in.
+        $('#login-panel').hide();
+        $('#status-panel').show();
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
         firebase.database().ref('students/' + uid).set({
@@ -22,13 +27,27 @@ $(document).ready(function() {
         });
         console.log(isAnonymous);
         console.log(uid);
+
+        var myRef = firebase.database().ref('students/' + user.uid);
+        myRef.on('value', function(snapshot) {
+            if (snapshot.val().word) {
+                $('#status-panel').hide();
+                $('#word-panel').show();
+                $('.word').text(snapshot.val().word);
+            }
+            console.log(snapshot.val());
+        });
+
         // ...
         } else {
+            $('#login-panel').show();
         // User is signed out.
         // ...
         }
         // ...
     });
+
+
     $("#sign-out").click(function(event) {
         firebase.auth().signOut().then(function() {
             console.log('Signed Out');
@@ -37,3 +56,11 @@ $(document).ready(function() {
         });
     });
 });
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });    
+}
