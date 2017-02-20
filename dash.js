87dda9c1b6a9bc8675c5eca8750141bdf6d05418
ashays <ashay.sheth@gmail.com>
2017-02-19 23:32:01 -0500

    Invite Students

--- dash.js
+++ dash.js
@@ -7,13 +7,21 @@ function onUserDataFunc() {
 	if (! userData.classesOwn && ! userData.classesIn) {
 		$('#no-classes-panel').show();
 	} else {
-		// To complete
 		if (userData.classesOwn) {
 			$('#my-classes-panel').show();
 			$('#my-classes-panel ul').empty();
 			userData.classesOwn.forEach(function(item, index) {
 				firebase.database().ref('classes/' + item + '/name').once('value', function(snapshot) {
 					$('#my-classes-panel ul').append('<li><a href="class.html?cid=' + item + '">' + snapshot.val() + '</a></li>');
+				});
+			});
+		}
+		if (userData.classesIn) {
+			$('#classes-panel').show();
+			$('#classes-panel ul').empty();
+			userData.classesIn.forEach(function(item, index) {
+				firebase.database().ref('classes/' + item + '/name').once('value', function(snapshot) {
+					$('#classes-panel ul').append('<li><a href="class.html?cid=' + item + '">' + snapshot.val() + '</a></li>');
 				});
 			});
 		}
