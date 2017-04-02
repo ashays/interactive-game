ebb72e5543c8f06751ce1e751024ba718d129282
ashays <ashay.sheth@gmail.com>
2017-04-01 20:03:07 -0400

    Basic set creation

--- new.js
+++ new.js
@@ -9,6 +9,17 @@ $(document).ready(function() {
 			displayError(error.message);
 		});;
 	});
+
+	$('#create-set').submit(function(e){
+		e.preventDefault();
+		var setname = $(e.target.name).val();
+		createSet(setname).then(function() {
+			// window.location.href = "set.html?qid=" + ;
+			window.location.href = "dash.html";
+		}, function(error) {
+			displayError(error.message);
+		});;
+	});
 });
 
 function createClass(classname) {
@@ -25,5 +36,22 @@ function createClass(classname) {
 	var updates = {};
 	updates['/classes/' + newClassKey] = newClass;
 	updates['/users/' + userData.uid + '/classesOwn/'] = classesOwn;
+	return firebase.database().ref().update(updates);
+}
+
+function createSet(setname) {
+	var newSet = {
+		name: setname,
+		owner: userData.uid
+	};
+	var newSetKey = firebase.database().ref().child('sets').push().key;
+	var mySets = [newSetKey]
+	if (userData.mySets) {
+		mySets = userData.mySets;
+		mySets.push(newSetKey);
+	}
+	var updates = {};
+	updates['/sets/' + newSetKey] = newSet;
+	updates['/users/' + userData.uid + '/mySets/'] = mySets;
 	return firebase.database().ref().update(updates);
 }
